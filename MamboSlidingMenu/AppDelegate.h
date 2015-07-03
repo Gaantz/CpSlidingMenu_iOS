d1a17c9c81ece79566cb376fb725661719c91836
cristian <cp@mambo.pe>
2015-07-03 11:45:30 -0500

    Initial Commit

---
+++ MamboSlidingMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MamboSlidingMenu
+//
+//  Created by Cristian on 3/07/15.
+//  Copyright (c) 2015 Cristian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MamboSlidingMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MamboSlidingMenu
+//
+//  Created by Cristian on 3/07/15.
+//  Copyright (c) 2015 Cristian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
